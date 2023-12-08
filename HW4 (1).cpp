96389544b754bd3a6a255a4830e1a3050359743f
mahdiiasadi <149286256+mahdiiasadi@users.noreply.github.com>
2023-12-08 15:02:03 -0800

    Add files via upload

---
+++ HW4 (1).cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float x, y;
+    cout << "Please enter value for x: ";
+    cin >> x;
+    cout << "Now enter value for y: ";
+    cin >> y;
+
+    cout << "x + y = " << x + y << "\n";
+    cout << "x - y = " << x - y << "\n";
+    cout << "x * y = " << x * y << "\n";
+    cout << "x / y = " << x / y << "\n";
+}
