d54ad2364c4614cf3fb54d9e0b79891c450d0dac
mahdiiasadi <149286256+mahdiiasadi@users.noreply.github.com>
2023-12-08 15:07:45 -0800

    Add files via upload

---
+++ HW5 (2).cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char x;
+    cout << "Please enter an English character: ";
+    cin >> x;
+
+    switch (x) {
+      case 'a':
+        cout << "VOWEL";
+        break;
+      case 'e':
+        cout << "VOWEL";
+        break;
+      case 'i':
+        cout << "VOWEL";
+        break;
+      case 'o':
+        cout << "VOWEL";
+      case 'w':
+        cout << "VOWEL";
+        break;
+    }
+}
